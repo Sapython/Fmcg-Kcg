0c7f9275469ad97a24e25b5ef2059fb4bf1caf7a
Rohan <112634088+RohanKaushik-alt@users.noreply.github.com>
2022-11-13 09:56:49 +0530

    Add pusrchase

--- src/app/add-purchase/add-purchase.page.ts
+++ src/app/add-purchase/add-purchase.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { SellerUtilsService } from 'src/services/SellerUtils/seller-utils.service';
 
 
 @Component({
@@ -10,20 +11,23 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AddPurchasePage implements OnInit {
 
   public addChildStock: FormGroup = new FormGroup({
-    Name: new FormControl('test'),
-    
+    name: new FormControl('test'),
+    quantity: new FormControl('test'),
   });
 
   public searchTextFrom: FormGroup = new FormGroup({
     searchText: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    address: new FormControl(''),
+
   });
 
-  public containers = [] ;
-  public textIsempty:boolean = false
+  public containers = [];
+  public textIsempty: boolean = false
   public data = ['Rohan', 'Mohan', 'Sohan', 'Ranjnikant'];
   public results = [...this.data];
 
-  constructor() { }
+  constructor(private seller: SellerUtilsService) { }
 
   ngOnInit() {
   }
@@ -40,12 +44,18 @@ export class AddPurchasePage implements OnInit {
   }
 
 
-  setValue(searchText:string){
+  setValue(searchText: string) {
     this.textIsempty = false;
     console.log(this.searchTextFrom.value);
     this.searchTextFrom.controls["searchText"].setValue(searchText);
   }
 
+
+  addPurchase() {
+    const data = [this.addChildStock.value, this.searchTextFrom.value]
+    console.log(data)
+    // this.seller.addPurchase(data)
+  }
 
 }
 
--- src/services/SellerUtils/seller-utils.service.ts
+++ src/services/SellerUtils/seller-utils.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { addDoc, collection, Firestore } from '@angular/fire/firestore';
+import { urls } from '../url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SellerUtilsService {
 
-  constructor() { }
+  constructor(public fs: Firestore) { }
+
+
+  // POST
+  public addPurchase(data) {
+    return addDoc(collection(this.fs, urls.purchase), data);
+  }
+
+
+
 }
