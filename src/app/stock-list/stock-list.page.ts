e96463f88e925b75db246db9c7d816076b1433a5
Saptam <saptampro2003@gmail.com>
2022-11-26 09:38:39 +0530

    Final Changes done

--- src/app/stock-list/stock-list.page.ts
+++ src/app/stock-list/stock-list.page.ts
@@ -13,32 +13,36 @@ import { RangeValue } from '@ionic/core';
 export class StockListPage implements OnInit {
   @ViewChild(IonModal) modal: IonModal;
   name: string;
-  public color:any;
-
-  lower:number = 0;
-  upper:number = 0
-  filters:any[] = []
+  public color: any;
+  lower: number = 0;
+  upper: number = 0;
+  filters: any[] = [];
   stocks: any[] = [];
   filteredStocks: any[] = [];
-  searchDebounceTimer:any;
-  ignoredFields = ["id","Name","Price","Quality","Quantity",'img']
+  searchDebounceTimer: any;
+  ignoredFields = ['id', 'Name', 'Price', 'Quality', 'Quantity', 'img'];
   constructor(private stockService: StocksService) {}
-  loading:boolean = true;
+  loading: boolean = true;
   ngOnInit() {
     this.stockList();
   }
-  debounceSearch(event:any){
+  async refresh(event) {
+    await this.stockList();
+    // alert("DONE")
+    event.target.complete();
+  }
+
+  debounceSearch(event: any) {
     try {
-      clearTimeout(this.searchDebounceTimer)
+      clearTimeout(this.searchDebounceTimer);
       this.searchDebounceTimer = setTimeout(() => {
         this.search(event);
-      },500)
-    } catch (error) {
-    }
+      }, 500);
+    } catch (error) {}
   }
 
   search(event: any) {
-    console.log("FIRED:",event)
+    console.log('FIRED:', event);
     if (event.target.value.length > 0) {
       let fuse = new Fuse(this.stocks, {
         keys: [
@@ -48,114 +52,121 @@ export class StockListPage implements OnInit {
           'Price',
           'Quality',
           'Quantity',
-          "Shade",
-          "StorageLocation"
+          'Shade',
+          'StorageLocation',
         ],
       });
       const res = fuse.search(event.target.value);
-      console.log("RESULTS:",res)
-      const stocksData:any[] = []
+      console.log('RESULTS:', res);
+      const stocksData: any[] = [];
       res.forEach((data: any) => {
         stocksData.push(data.item);
       });
       this.filteredStocks = JSON.parse(JSON.stringify(stocksData));
     } else {
-      this.filteredStocks = []
+      this.filteredStocks = [];
     }
   }
-  
-  stockList() {
-    this.stockService.Stocks().then((res) => {
-      res.forEach((element: any) => {
-        this.stocks.push({
-          ...element.data(),
-          id: element.id,
-        });
-      });
 
-    }).finally(() => {
-      this.loading=false
-      let itemAttributes = {}
-      this.stocks.forEach((item) => {
-        for (var key of Object.keys(item)){
-          if (this.ignoredFields.includes(key) || Number(item[key])){
-            continue
-          }
-          if (itemAttributes[key]){
-            itemAttributes[key].counter++;
-            // check if item is already in the array
-            var found = false;
-            itemAttributes[key].items.forEach((itemVal:any)=>{
-              if (itemVal.value.toLowerCase() == item[key].toLowerCase()){
-                found = true
-              }
-            })
-            if (!found){
-              itemAttributes[key].items.push({value:item[key],key:key,counter:1,active:false})
-            }
-          } else {
-            itemAttributes[key] = {
-              counter: 1,
-              title:key,
-              value: item[key],
-              items: [{value:item[key],active:false,counter:1,key:key}],
-              active: false
-            }
-          }
+  async stockList() {
+    const res = await this.stockService.Stocks();
+    this.stocks = [];
+    res.forEach((element: any) => {
+      this.stocks.push({
+        ...element.data(),
+        id: element.id,
+      });
+    });
+    this.loading = false;
+    let itemAttributes = {};
+    // alert("DONE 1")
+    this.stocks.forEach((item) => {
+      for (var key of Object.keys(item)) {
+        if (this.ignoredFields.includes(key) || Number(item[key])) {
+          continue;
         }
-      })
-      console.log(itemAttributes)
-      // sort itemAttributes by key name
-      let sortedItemAttributes = {}
-      Object.keys(itemAttributes).sort().forEach(function(key) {
+        if (itemAttributes[key]) {
+          itemAttributes[key].counter++;
+          // check if item is already in the array
+          var found = false;
+          itemAttributes[key].items.forEach((itemVal: any) => {
+            if (itemVal.value == item[key]) {
+              found = true;
+            }
+          });
+          if (!found) {
+            itemAttributes[key].items.push({
+              value: item[key],
+              key: key,
+              counter: 1,
+              active: false,
+            });
+          }
+        } else {
+          itemAttributes[key] = {
+            counter: 1,
+            title: key,
+            value: item[key],
+            items: [{ value: item[key], active: false, counter: 1, key: key }],
+            active: false,
+          };
+        }
+      }
+    });
+    // alert("DONE 2")
+    console.log(itemAttributes);
+    // sort itemAttributes by key name
+    let sortedItemAttributes = {};
+    Object.keys(itemAttributes)
+      .sort()
+      .forEach(function (key) {
         sortedItemAttributes[key] = itemAttributes[key];
       });
-      
-      let filteredKeys = []
-      Object.values(itemAttributes).forEach((item:any)=>{
-        if (item.counter > 2 && item.counter < 10){
-          filteredKeys.push(item)
-        }
-      })
-      console.log(filteredKeys)
-      this.filters = filteredKeys
+
+    let filteredKeys = [];
+    Object.values(itemAttributes).forEach((item: any) => {
+      if (item.counter > 2 && item.counter < 10) {
+        filteredKeys.push(item);
+      }
     });
+    console.log(filteredKeys);
+    this.filters = filteredKeys;
+    // alert("DONE 3")
   }
 
-  filterOptions(){
-    let filteredStocks = []
-    this.filters.forEach((filter:any)=>{
-      filter.items.forEach((item:any)=>{
-        if (item.active){
-          filteredStocks.push(item)
+  filterOptions() {
+    let filteredStocks = [];
+    this.filters.forEach((filter: any) => {
+      filter.items.forEach((item: any) => {
+        if (item.active) {
+          filteredStocks.push(item);
         }
-      })
-    })
-    console.log(filteredStocks)
-    if (this.filteredStocks.length > 0){
-      this.filteredStocks = this.filteredStocks.filter((stock:any)=>{
-        let found = false
-        filteredStocks.forEach((filter:any)=>{
-          if (stock[filter.key].toLowerCase() == filter.value.toLowerCase()){
-            found = true
+      });
+    });
+    console.log(filteredStocks);
+    if (this.filteredStocks.length > 0) {
+      this.filteredStocks = this.filteredStocks.filter((stock: any) => {
+        let found = false;
+        filteredStocks.forEach((filter: any) => {
+          if (stock[filter.key].toLowerCase() == filter.value.toLowerCase()) {
+            found = true;
           }
-        })
-        return found
-      })
+        });
+        return found;
+      });
     } else {
-      this.filteredStocks = this.stocks.filter((stock:any)=>{
-        let found = false
-        filteredStocks.forEach((filter:any)=>{
-          if (stock[filter.key].toLowerCase() == filter.value.toLowerCase()){
-            found = true
+      this.filteredStocks = this.stocks.filter((stock: any) => {
+        let found = false;
+        filteredStocks.forEach((filter: any) => {
+          if (stock[filter.key].toLowerCase() == filter.value.toLowerCase()) {
+            found = true;
           }
-        })
-        return found
-      })
+        });
+        return found;
+      });
     }
-
   }
-  
+
   cancel() {
     this.modal.dismiss(null, 'cancel');
   }
@@ -168,20 +179,18 @@ export class StockListPage implements OnInit {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
   }
 
-  
   pinFormatter(value: number) {
     return `${value} pcs`;
   }
 
-
-  log(event:any){
-    console.log(event)
+  log(event: any) {
+    console.log(event);
     this.lower = event.detail.value.lower;
     this.upper = event.detail.value.upper;
   }
 
-  chipChange(){
-    this.color = !(this.color)
-    console.log(this.color)
+  chipChange() {
+    this.color = !this.color;
+    console.log(this.color);
   }
 }
