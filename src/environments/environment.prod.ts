94960ef00fdc119658c5cf36feab0183e7bf9e71
Sapython <saptampro2003@gmail.com>
2024-11-17 17:44:49 +0530

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'fmcg-kcg',
+    appId: '1:738798707302:web:3f816ae17fa684f04403a7',
+    storageBucket: 'fmcg-kcg.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'REPLACEMENT_STRING',
+    authDomain: 'fmcg-kcg.firebaseapp.com',
+    messagingSenderId: '738798707302',
+    measurementId: 'G-YD8HFMVRSY',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'fmcg-kcg',
+    appId: '1:738798707302:web:3f816ae17fa684f04403a7',
+    storageBucket: 'fmcg-kcg.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'REPLACEMENT_STRING',
+    authDomain: 'fmcg-kcg.firebaseapp.com',
+    messagingSenderId: '738798707302',
+    measurementId: 'G-YD8HFMVRSY',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
